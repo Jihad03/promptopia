d1ceb626feba5db952aac71d7562a0307c010424
jihadChowdhory <mohammadjihad0333@gmail.com>
2023-06-05 09:44:27 +0600

    Completed the project

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,41 +2,58 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
-import MyProfile from '@components/Profile';
+import MyProfile from "@components/Profile";
 
 const page = () => {
-    const {data:session} = useSession();
+  const { data: session } = useSession();
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-          const response = await fetch(`/api/users/${session?.user.id}/posts`);
-          const data = await response.json();
-    
-          setPosts(data);
-        };
-    
-        if(session?.user.id) fetchPosts();
-      }, []);
-    
-    const handleEdit = () => {
+      setPosts(data);
+    };
 
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
     }
+  };
 
-    const handleDelete = async () => {
-
-    }
-
-    return <MyProfile
-        name="My"
-        desc="Welcome to your personalized MyProfile page"
-        data={posts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
+  return (
+    <MyProfile
+      name="My"
+      desc="Welcome to your personalized Profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
     />
-}
+  );
+};
 
 export default page;
